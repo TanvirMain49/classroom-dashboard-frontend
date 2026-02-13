7bf98292f119d511efdd370dcd14b02de6ddf9bf
TanvirMain49 <tanvirmahin607@gmail.com>
2026-02-13 20:40:06 +0600

    fixed mejor bugs in the codes from auth.ts, data.ts and auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
     baseURL:`${BACKEND_BASE_URL}auth`,
     user: {
-        additionalField:{
+        additionalFields:{
             role: {
                 type: USER_ROLES,
                 required: true,
